4d1ddf945f193a05981cebd17cdc8c685ec49467
Lijo Daniel <ldaniel@healthedge.com>
2016-05-09 16:33:47 -0400

    method created

--- RotateArray/src/com/ld/RotateArray.java
+++ RotateArray/src/com/ld/RotateArray.java
@@ -4,11 +4,17 @@ public class RotateArray {
 
 	public static void main(String[] args) {
 		int[] numArray = {1,2,3,4,5,6,7};
+		int rotateBy = 3;
 		
-		rotateArray(numArray);
+		rotateArray(numArray, rotateBy);
 	}
 	
-	private static void rotateArray(int[] numArray) {
+	private static void rotateArray(int[] numArray, int rotateBy) {
+		if (rotateBy > numArray.length) {
+			rotateBy = rotateBy % numArray.length;
+		}
+		
+		
 		
 	}
 }
