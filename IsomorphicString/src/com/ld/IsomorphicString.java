7e586d8f86aed5e8367fca8764a0451d0e858e7d
lijod <lijo_daniel90@yahoo.com>
2016-05-10 10:59:55 -0400

    IsomorphicString initialized

---
+++ IsomorphicString/src/com/ld/IsomorphicString.java
@@ -0,0 +1,16 @@
+package com.ld;
+
+public class IsomorphicString {
+	
+	public static void main (String[] args) {
+		
+		
+		
+	}
+	
+	private static boolean isIsomorphic (String s, String t) {
+		
+		return false;
+	}
+	
+}
