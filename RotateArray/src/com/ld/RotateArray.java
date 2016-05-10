2e783a1bd72d4246658d58c07b47c09b769229e0
lijod <lijo_daniel90@yahoo.com>
2016-05-10 09:48:10 -0400

    RotateArray completed

--- RotateArray/src/com/ld/RotateArray.java
+++ RotateArray/src/com/ld/RotateArray.java
@@ -3,10 +3,14 @@ package com.ld;
 public class RotateArray {
 
 	public static void main(String[] args) {
-		int[] numArray = {1,2,3,4,5,6,7};
-		int rotateBy = 3;
+		int[] numArray = {0, 1, 2, 3, 4, 5, 6};
+		int rotateBy = 8;
 		
 		rotateArray(numArray, rotateBy);
+		
+		for(int num : numArray) {
+			System.out.print(num + " ");
+		}
 	}
 	
 	private static void rotateArray(int[] numArray, int rotateBy) {
@@ -14,7 +18,21 @@ public class RotateArray {
 			rotateBy = rotateBy % numArray.length;
 		}
 		
+		reversePartOfArray(numArray, numArray.length - rotateBy, numArray.length - 1);
+		reversePartOfArray(numArray, 0, numArray.length - rotateBy - 1);
+		reversePartOfArray(numArray, 0, numArray.length - 1);
 		
+	}
+	
+	private static void reversePartOfArray(int[] numArray, int left, int right) {
+		
+		while(left < right) {
+			int temp = numArray[left];
+			numArray[left] = numArray[right];
+			numArray[right] = temp;
+			left++;
+			right--;
+		}
 		
 	}
 }
