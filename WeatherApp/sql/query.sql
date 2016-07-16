306fbec9f081de518383eb28e146e2209df3a27e
Lijo Daniel <lijodaniel@Lijos-MacBook-Pro.local>
2016-07-16 14:45:43 -0400

    mysql driver version changed

--- WeatherApp/sql/query.sql
+++ WeatherApp/sql/query.sql
@@ -11,7 +11,8 @@ create table weather_preferences
     user_id int,
     zip_code VARCHAR(10) not null,
     is_local boolean not null,
-    name VARCHAR(10) not null,
+    name VARCHAR(100) not null,
+    PRIMARY KEY (user_id, zip_code),
     FOREIGN KEY (user_id) 
     REFERENCES app_user(user_id)
     On DELETE CASCADE
